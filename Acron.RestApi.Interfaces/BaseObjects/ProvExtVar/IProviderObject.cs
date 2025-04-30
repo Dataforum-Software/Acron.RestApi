6ff3be36aee560d2ae2457f613eaa55428d474b6
fzimmermann-dafo <fzimmermann@dataforum.de>
2025-04-30 13:35:21 +0200

    Merged Changes from 10.0 dev branch

--- Acron.RestApi.Interfaces/BaseObjects/ProvExtVar/IProviderObject.cs
+++ Acron.RestApi.Interfaces/BaseObjects/ProvExtVar/IProviderObject.cs
@@ -47,9 +47,13 @@ namespace Acron.RestApi.Interfaces.BaseObjects
       public const int DrvWinccUni64 = 8000036;
       public const int DrvSysInfo64 = 8000037;
       public const int DrvOpcclientua64 = 8000038; // NEU - 64Bit und immer mit TS
+      public const int DrvWincc80 = 8000039; // NEU 10.0 - Nachfolger von DrvWincc72
+      public const int DrvOpcclientuats64 = 8000040; // NEU 10.0 - 64Bit
+      public const int DrvOpcclientuaha64 = 8000041; // NEU 10.0 - 64Bit
+      public const int DrvOpcclientuaac64 = 8000042; // NEU 10.0 - 64Bit
 
       public const int MinDriverId = DrvZenon;
-      public const int MaxDriverId = DrvOpcclientua64; // !!! Always compensate the last driver !!!
+      public const int MaxDriverId = DrvOpcclientuaac64; // !!! Always compensate the last driver !!!
 
       #endregion Driver IDs
    }
--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2023 DataForum Software GmbH
+Copyright (c) 2024 DataForum Software GmbH
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
